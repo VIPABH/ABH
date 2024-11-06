cf465fbcf139e49dbc0758064e54b693efae5055
VIPABH <a891983.2@gmail.com>
2024-11-06 22:12:34 +0300

    Add files via upload

---
+++ exampleconfig.py
@@ -0,0 +1,11 @@
+from sample_config import Config
+class Development(Config):
+    APP_ID = 
+    API_HASH = ""
+    ALIVE_NAME = "ابـ،ـن،هـ.ـاشـ.ـم ✘"
+    DB_URI = "postgresql://postgres:your_password@localhost:5432/aljoke"
+    STRING_SESSION = ""
+    TG_BOT_TOKEN = ""
+    COMMAND_HAND_LER = "."
+    SUDO_USERS = []
+    SUDO_COMMAND_HAND_LER = "."
