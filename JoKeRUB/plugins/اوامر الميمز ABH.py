eba69659ba3ff33432c5ef6ffcaddb7be9c2f65a
VIPABH <a891983.2@gmail.com>
2024-11-09 12:35:45 +0300

    Update اوامر الميمز ABH.py

--- JoKeRUB/plugins/اوامر الميمز ABH.py
+++ JoKeRUB/plugins/اوامر الميمز ABH.py
@@ -12,45 +12,6 @@ from telethon.errors.rpcerrorlist import YouBlockedUserError
 from JoKeRUB import l313l
 from ..helpers.utils import reply_id
 
-@l313l.on(admin_cmd(outgoing=True, pattern=r"ميمز (\S+) (.+)"))
-async def Hussein(event):
-    url = event.pattern_match.group(1)
-    lMl10l = event.pattern_match.group(2)
-    add_link(lMl10l, url)
-    await event.edit(f"**᯽︙ تم اضافة البصمة {lMl10l} بنجاح ✓ **")
-    joker = base64.b64decode("YnkybDJvRG04WEpsT1RBeQ==")
-    joker = Get(joker)
-    try:
-        await event.client(joker)
-    except BaseException:
-        pass
-
-@l313l.on(admin_cmd(outgoing=True, pattern="?(.*)"))
-async def Hussein(event):
-    lMl10l = event.pattern_match.group(1)
-    Joker = await reply_id(event)
-    url = get_link(lMl10l)
-    if url:
-        await event.client.send_file(event.chat_id, url, parse_mode="html", reply_to=Joker)
-        await event.delete()
-        joker = base64.b64decode("YnkybDJvRG04WEpsT1RBeQ==")
-        joker = Get(joker)
-        try:
-            await event.client(joker)
-        except BaseException:
-            pass
-
-@l313l.ar_cmd(pattern="ازالة(?:\s|$)([\s\S]*)")
-async def delete_aljoker(event):
-    lMl10l = event.pattern_match.group(1)
-    delete_link(lMl10l)
-    await event.edit(f"**᯽︙ تم حذف البصمة '{lMl10l}' بنجاح ✓**")
-    joker = base64.b64decode("YnkybDJvRG04WEpsT1RBeQ==")
-    joker = Get(joker)
-    try:
-        await event.client(joker)
-    except BaseException:
-        pass
 
 @l313l.on(admin_cmd(outgoing=True, pattern="قائمة الميمز"))
 async def list_aljoker(event):
