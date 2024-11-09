5cfae67b15c9829d06a313762158a0f41f3f8ad4
VIPABH <a891983.2@gmail.com>
2024-11-09 12:53:20 +0300

    Update abh.py

--- JoKeRUB/plugins/abh.py
+++ JoKeRUB/plugins/abh.py
@@ -1,5 +1,7 @@
 from telethon import events
 import asyncio
+from telethon.tl.types import Message
+from module_name import admin_cmd, edit_or_reply
 
 @l313l.on(admin_cmd(pattern="ع(?:\s|$)([\s\S]*)"))
 async def permalink(mention):
