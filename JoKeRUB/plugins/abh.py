ebec8fb89cb1f0bf2f6811600fcd59d4326b3b0b
VIPABH <a891983.2@gmail.com>
2024-11-09 12:56:16 +0300

    Delete JoKeRUB/plugins/abh.py

--- JoKeRUB/plugins/abh.py
+++
@@ -1,8 +0,0 @@
-from telethon import events
-import asyncio
-from telethon.tl.types import Message
-from module_name import admin_cmd, edit_or_reply
-
-@l313l.on(admin_cmd(pattern="ع(?:\s|$)([\s\S]*)"))
-async def permalink(mention):
-    await edit_or_reply(mention, f"عليكم السلام , اهلا")
