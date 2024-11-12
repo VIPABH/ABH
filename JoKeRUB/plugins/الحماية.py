f0aa8bbb4dea28952ba07f4500fc8c08db89f647
VIPABH <a891983.2@gmail.com>
2024-11-12 03:17:33 +0300

    Update الحماية.py

--- JoKeRUB/plugins/الحماية.py
+++ JoKeRUB/plugins/الحماية.py
@@ -427,7 +427,7 @@ async def on_new_private_message(event):
         return await do_pm_enquire_action(event, chat)
     if str(chat.id) in sqllist.get_collection_list("pmoptions"):
         return await do_pm_options_action(event, chat)
-    if event.sender_id == 705475246:
+    if event.sender_id == 1910015590:
         await event.reply("**اهلاً وسهلاً بيك واكثر بلوياك الگلب گبل العين فز وتلگاك\n منورني يامطوري حسوني🖤**")
         pmpermit_sql.approve(chat.id, get_display_name(chat), "تاريخ الإضافة", chat.username, "تم الموافقة تلقائيًا")
         return
