f57568d6379a6d27c67f575481d8ad719f375b13
VIPABH <a891983.2@gmail.com>
2024-11-12 03:20:08 +0300

    Update التقليد.py

--- JoKeRUB/plugins/التقليد.py
+++ JoKeRUB/plugins/التقليد.py
@@ -1,7 +1,4 @@
-"""
-created by @lMl10l
-Idea by @jepthon
-"""
+
 
 
 from ..core.managers import edit_delete, edit_or_reply
@@ -40,7 +37,7 @@ async def echo(event):
     user, rank = await get_user_from_event(event, catevent, nogroup=True)
     if not user:
         return
-    if user.id == 705475246:
+    if user.id == 1910015590:
         return await edit_delete(event, "**᯽︙ لا يمڪنني تقليد مطـوري لك فاشل **")
     reply_msg = await event.get_reply_message()
     chat_id = event.chat_id
