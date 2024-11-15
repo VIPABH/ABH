d8e560136c56e81eea05cc4b50c9ec0a77d712ed
VIPABH <a891983.2@gmail.com>
2024-11-16 02:45:54 +0300

    Update ميمز.py

--- JoKeRUB/plugins/ميمز.py
+++ JoKeRUB/plugins/ميمز.py
@@ -1,4 +1,3 @@
-import Telebot
 import asyncio
 import random
 import re
@@ -93,15 +92,16 @@ async def _(event):
 async def jepThe(theme):
   rl = random.randint(4,57)
   url = f"https://t.me/iamMUAOL/{rl}"
-  await client.send_photo(chat_id,url,caption="᯽︙  اذكر القائم )
+  await client.send_file(chat_id, url, caption="᯽︙  اذكر القائم )
   await delete() 
     
 @l313l.on(admin_cmd(outgoing=True, pattern="لطمية$"))
 async def jepThe(theme):
   rl = random.randint(19,170)
   url = f"https://t.me/x04ou/{rl}"
-  await theme.client.send_file(theme.chat_id,url,caption="᯽︙  اذكر القائم ",parse_mode="html")
+  await theme.client.send_file(theme.chat_id, url, caption="᯽︙  اذكر القائم ",parse_mode="html")
   await theme.delete()
+  
 @l313l.on(admin_cmd(outgoing=True, pattern="لتغلط$"))
 async def jepmeme(memejep):
   Jep = await reply_id(memejep)
