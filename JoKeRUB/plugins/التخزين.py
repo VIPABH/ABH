c0cefe8930950aea93c1a396233684a3af4c3205
VIPABH <a891983.2@gmail.com>
2024-11-17 21:15:02 +0300

    Update التخزين.py

--- JoKeRUB/plugins/التخزين.py
+++ JoKeRUB/plugins/التخزين.py
@@ -81,7 +81,7 @@ async def log_tagged_messages(event):
         return
     if (
         (no_log_pms_sql.is_approved(hmm.id))
-        or (Config.PM_LOGGER_GROUP_ID == -1001852592581)
+        or (Config.PM_LOGGER_GROUP_ID == -100)
         or ("on" in AFK_.USERAFK_ON)
         or (await event.get_sender() and (await event.get_sender()).bot)
     ):
@@ -114,26 +114,14 @@ async def log_tagged_messages(event):
             link_preview=False,
         )
 
-import asyncio
-from telethon.tl.types import Message
-from JoKeRUB import l313l
-from JoKeRUB.core.logger import logging
-from telethon.errors.rpcerrorlist import MessageNotModifiedError
-from ..Config import Config
-from ..core.managers import edit_delete
-from ..helpers.tools import media_type
-from ..helpers.utils import _format
-from ..sql_helper import no_log_pms_sql
-from ..sql_helper.globals import addgvar, gvarstatus
-from . import BOTLOG, BOTLOG_CHATID
 
 @l313l.ar_cmd(
-    pattern="خاص(?:\s|$)([\s\S]*)",
-    command=("خاص", plugin_category),
+    pattern="خزن(?:\s|$)([\s\S]*)",
+    command=("خزن", plugin_category),
     info={
         "header": "To log the replied message to bot log group so you can check later.",
         "الاسـتخـدام": [
-            "{tr}خاص",
+            "{tr}خزن",
         ],
     },
 )
@@ -142,7 +130,7 @@ async def log(log_text):
     if BOTLOG:
         if log_text.reply_to_msg_id:
             reply_msg = await log_text.get_reply_message()
-            await reply_msg.forward_to("me")
+            await reply_msg.forward_to(BOTLOG_CHATID)
         elif log_text.pattern_match.group(1):
             user = f"#التخــزين / ايـدي الدردشــه : {log_text.chat_id}\n\n"
             textx = user + log_text.pattern_match.group(1)
@@ -150,11 +138,12 @@ async def log(log_text):
         else:
             await log_text.edit("**⌔┊بالــرد على اي رسـاله لحفظهـا في كـروب التخــزين**")
             return
-        await log_text.edit("**⌔┊تـم الحفـظ في كـروب الخاص.. بنجـاح ✓**")
+        await log_text.edit("**⌔┊تـم الحفـظ في الخاص .. بنجـاح ✓**")
     else:
         await log_text.edit("**⌔┊عـذراً .. هـذا الامـر يتطلـب تفعيـل فـار التخـزين اولاً**")
     await asyncio.sleep(2)
     await log_text.delete()
+
 
 @l313l.ar_cmd(
     pattern="تفعيل التخزين$",
