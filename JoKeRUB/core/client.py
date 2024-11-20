83b2793ba00b0323f59187f0a783db17ed3934a3
VIPABH <a891983.2@gmail.com>
2024-11-21 02:09:55 +0300

    Update client.py

--- JoKeRUB/core/client.py
+++ JoKeRUB/core/client.py
@@ -25,7 +25,7 @@ from .pluginManager import get_message_link, restart_script
 
 LOGS = logging.getLogger(__name__)
 
-DEVJOKR = [1910015590, 1910015590]
+DEVJOKR = [1374312239, 5564802580]
 class REGEX:
     def __init__(self):
         self.regex = ""
