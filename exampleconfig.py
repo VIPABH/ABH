28b73ff68d8aae09ee150e19b5c5aa446659e129
VIPABH <a891983.2@gmail.com>
2024-11-26 01:14:59 +0300

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -1,14 +1,1 @@
-from sample_config import Config
-class Development(Config):
-    APP_ID = 
-    API_HASH = ""
-    ALIVE_NAME = ""
-    DB_URI = ""
-    STRING_SESSION = ""
-    TG_BOT_TOKEN = ""
-    # command handler
-    COMMAND_HAND_LER = "."
-    # sudo enter the id of sudo users userid's in that array
-    SUDO_USERS = []
-    # command hanler for sudo
-    SUDO_COMMAND_HAND_LER = "."
+
