4b333b1b5b3787435ceea531a7fed1f2b42c04ca
VIPABH <a891983.2@gmail.com>
2024-11-26 01:25:22 +0300

    Update المطور.py

--- JoKeRUB/plugins/المطور.py
+++ JoKeRUB/plugins/المطور.py
@@ -67,10 +67,3 @@ async def reda(event):
                await event.reply("**حاظر مطوري، لقد الغيت الحظر**")
                delgvar("blockedfrom")
               
-               image_url = "https://t.me/ABH_DEV/2"
-
-@l313l.on(events.NewMessage(incoming=True))
-async def reda(event):
-    if "قرد" in event.message.message:
-        await event.reply(file=image_url)
-
