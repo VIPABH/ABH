dcca61e476715900f28898d8d3b52657d9bd3acf
VIPABH <a891983.2@gmail.com>
2024-12-03 02:01:20 +0300

    Update تفرعات الاوامر.py

--- JoKeRUB/plugins/تفرعات الاوامر.py
+++ JoKeRUB/plugins/تفرعات الاوامر.py
@@ -5,7 +5,7 @@ from JoKeRUB import l313l
 from ..core.managers import edit_or_reply
 
 plugin_category = "extra"
-banned_users = [1510396227, 6048901890]  
+banned_users = [1510396227, 6844153802]  
 
 @l313l.ar_cmd(incoming=True, func=lambda e: "السلام عليكم" in e.text.lower() or "سلام عليكم" in e.text.lower(), edited=False)
 async def reply_salam(event):
