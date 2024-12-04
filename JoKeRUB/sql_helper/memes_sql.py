8961318793f994b2fbf90e048a525c03f959950a
VIPABH <a891983.2@gmail.com>
2024-12-04 22:09:47 +0300

    Update memes_sql.py

--- JoKeRUB/sql_helper/memes_sql.py
+++ JoKeRUB/sql_helper/memes_sql.py
@@ -1,5 +1,5 @@
 try:
-    from . import BASE, SESSION
+    from . import BASE, SESSION, engine
 except ImportError as e:
     raise Exception("Hello!") from e
 from sqlalchemy import Column, String
@@ -10,7 +10,7 @@ class AljokerLink(BASE):
     def __init__(self, key, url):
         self.key = str(key)
         self.url = str(url)
-AljokerLink.__table__.create(checkfirst=True)
+AljokerLink.__table__.create(bind=engine, checkfirst=True)
 def get_link(key):
     link = SESSION.query(AljokerLink).get(str(key))
     return link.url if link else None
@@ -18,3 +18,8 @@ def add_link(key, url):
     link = AljokerLink(str(key), str(url))
     SESSION.add(link)
     SESSION.commit()
+def delete_link(key):
+    link = SESSION.query(AljokerLink).get(str(key))
+    if link:
+        SESSION.delete(link)
+        SESSION.commit()
