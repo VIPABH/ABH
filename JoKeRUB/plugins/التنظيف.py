b0ab08d0fd670f63ce3a7d879ce3ba609bfd0f1c
VIPABH <a891983.2@gmail.com>
2024-12-05 02:59:30 +0300

    Update التنظيف.py

--- JoKeRUB/plugins/التنظيف.py
+++ JoKeRUB/plugins/التنظيف.py
@@ -393,7 +393,7 @@ async def fastpurger(event):  # sourcery no-metrics
                 error += f"\n᯽︙ `{ty}`  : هـذه أضافـة خاطئـة "
     elif input_str.isnumeric():
         async for msg in event.client.iter_messages(chat, limit=int(input_str) + 1):
-            count += 1
+            count = 0
             msgs.append(msg)
             if len(msgs) == 50:
                 await event.client.delete_messages(chat, msgs)
