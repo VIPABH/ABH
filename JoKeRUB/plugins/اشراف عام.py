41961c01b58f8e68113e03b75779cd757ba3c84a
VIPABH <a891983.2@gmail.com>
2024-12-04 23:41:07 +0300

    Update اشراف عام.py

--- JoKeRUB/plugins/اشراف عام.py
+++ JoKeRUB/plugins/اشراف عام.py
@@ -1,5 +1,4 @@
-#    جميع الحقوق محفوظة كتابة وتعديل  :   @lMl10l
-#    اخمط مع ذكر الحقوق غيرها انت مطور فاشل
+
 marculs=9
 from telethon.errors.rpcerrorlist import (UserIdInvalidError,
                                             MessageTooLongError)
