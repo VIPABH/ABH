2615f66b57b36573cdf771d3581125253492efe7
VIPABH <a891983.2@gmail.com>
2024-12-11 00:30:55 +0300

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -1,1 +1,10 @@
-
+from sample_config import Config
+class Development(Config):
+    APP_ID = 
+    API_HASH = ""
+    ALIVE_NAME = ""
+    # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
+    DB_URI = ""
+    COMMAND_HAND_LER = "."
+    SUDO_USERS = []
+    SUDO_COMMAND_HAND_LER = "."
