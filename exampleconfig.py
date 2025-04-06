2a565f210b944b1e42165f179ec1dbd2e1000b5e
VIPABH <a891983.2@gmail.com>
2025-04-06 15:26:20 +0300

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -5,7 +5,7 @@ class Development(Config):
     ALIVE_NAME = ""
     STRING_SESSION = 
     TG_BOT_TOKEN = 
-    DB_URI = "postgresql://postgres:your_password@localhost:5432/num"
+    DB_URI = "postgresql://postgres:your_password@localhost:5432/y"
     COMMAND_HAND_LER = "."
     SUDO_USERS = []
     SUDO_COMMAND_HAND_LER = "."
