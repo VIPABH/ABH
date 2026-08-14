371385e799634b253f23d40163e3f8bb563e2361
VIPABH <a891983.2@gmail.com>
2026-08-14 15:14:53 +0300

    Update button_bot.py

--- button_bot.py
+++ button_bot.py
@@ -1,13 +1,17 @@
 from helpers import *
 from client import *
+await asyncio
 @BUTTON_BOT.on(events.NewMessage(pattern=r'^/start$'))
 async def start(e):
     if not e.is_private:return
     user = await is_user(e)
     if not is_user:return
     photo = await get_profile_photo(e)
-    await BUTTON_BOT.send_file(e.chat_id, file=photo, caption=f'اهلا عزيزي ( {await ment(e)} ) اني بوت مال ازرار استخدامي سهل و بسيط ارسل `الاوامر`')
-@BUTTON_BOT.on(events.NewMessage(pattern=r'^الاوامر$'))
+    if photo:
+        await BUTTON_BOT.send_file(e.chat_id, file=photo, caption=f'اهلا عزيزي ( {await ment(e)} ) اني بوت مال ازرار استخدامي سهل و بسيط ارسل `الاوامر`', reply_to=e.id)
+    else:
+        await BUTTON_BOT.send_message(e.chat_id, message=f'اهلا عزيزي ( {await ment(e)} ) اني بوت مال ازرار استخدامي سهل و بسيط ارسل `الاوامر`', reply_to=e.id)
+@ABH.on(events.NewMessage(pattern=r'^الاوامر$'))
 async def command(e):
     if not e.is_private:return
     await e.reply(f'''
