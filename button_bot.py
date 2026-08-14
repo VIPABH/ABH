c26df2f90b318a8c7e31f342c883197e794548e0
VIPABH <a891983.2@gmail.com>
2026-08-14 15:37:06 +0300

    Update button_bot.py

--- button_bot.py
+++ button_bot.py
@@ -9,7 +9,7 @@ async def start(e):
     if photo:
         await BUTTON_BOT.send_file(e.chat_id, file=photo, caption=f'اهلا عزيزي ( {await ment(e)} ) اني بوت مال ازرار استخدامي سهل و بسيط ارسل `الاوامر`', reply_to=e.id)
     else:
-        await BUTTON_BOT.send_message(e.chat_id, message=f'اهلا عزيزي ( {await ment(e)} ) اني بوت مال ازرار استخدامي سهل و بسيط ارسل `الاوامر`', reply_to=e.id)
+        await BUTTON_BOT.send_message(e.chat_id, message=f'اهلا عزيزي ( {await ment(e)} ) اني بوت مال ازرار استخدامي سهل و بسيط \n ارسل `الاوامر`', reply_to=e.id)
 @BUTTON_BOT.on(events.NewMessage(pattern=r'^الاوامر$'))
 async def command(e):
     if not e.is_private:return
