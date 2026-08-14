380702b7ee668222e7912b7be8218bd7f6edfc36
VIPABH <a891983.2@gmail.com>
2026-08-14 15:28:10 +0300

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -3,10 +3,10 @@ from telethon import TelegramClient, events, connection, Button
 from telethon.errors import UserNotParticipantError
 from client import *
 import asyncio
-ABH = 1910015590
+wfffp = 1910015590
 def hint(message, **kwargs):
     task = asyncio.create_task(
-        BUTTON_BOT.send_message(ABH, message, **kwargs)
+        BUTTON_BOT.send_message(message, **kwargs)
     )
     task.add_done_callback(_log_task_result)
     return task
@@ -48,8 +48,8 @@ async def is_user(e):
 async def get_profile_photo(id, user=None):
     photos = []
     try:
-        user = user if user else await ABH.get_entity(id)
-        photos = await ABH.get_profile_photos(user, limit=1)
+        user = user if user else await BUTTON_BOT.get_entity(id)
+        photos = await BUTTON_BOT.get_profile_photos(user, limit=1)
         if photos:
             return photos[0]
         else:
@@ -73,7 +73,7 @@ async def ment(entity):
         if user_id in mentions_dict:
             return mentions_dict[user_id]
         if not hasattr(entity, 'first_name') or (hasattr(entity, 'id') and entity.id != user_id):
-            entity = await ABH.get_entity(user_id)
+            entity = await BUTTON_BOT.get_entity(user_id)
             name = getattr(entity, 'first_name', 'مستخدم') or 'مستخدم'
         if user_id not in mentions_dict:
             mentions_dict[user_id] = f"[{name}](tg://user?id={user_id})"
