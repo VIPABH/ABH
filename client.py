60087598ce7fc464247ef0f4c312a10f64b802f2
VIPABH <a891983.2@gmail.com>
2026-08-14 10:13:13 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,1 +1,13 @@
-
+from telethon import events, TelegramClient, connection
+api_id = int(os.getenv("API_ID") or config['anymous'].get('api_id'))
+api_hash = os.getenv("API_HASH") or config['anymous'].get('api_hash')
+bot_token = os.getenv("BOT_TOKEN") or config['anymous'].get('bot_token')
+BUTTON_BOT = TelegramClient(
+    "BUTTON_BOT", 
+    api_id, 
+    api_hash,
+    connection=connection.ConnectionTcpFull,
+    sequential_updates=False,
+    auto_reconnect=True,
+    connection_retries=None
+)
