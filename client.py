85d209b7565c6e9753beaf46569080d21b6fe0fa
VIPABH <a891983.2@gmail.com>
2026-08-14 11:41:36 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,15 +1,21 @@
 from telethon import events, TelegramClient, connection
-import redis
-api_id = int(os.getenv("API_ID")
+import os, redis
+api_id = int(os.getenv("API_ID"))
 api_hash = os.getenv("API_HASH")
 bot_token = os.getenv("BUTTON_BOT")
 BUTTON_BOT = TelegramClient(
-    "BUTTON_BOT", 
-    api_id, 
+    "BUTTON_BOT",
+    api_id,
     api_hash,
     connection=connection.ConnectionTcpFull,
     sequential_updates=False,
     auto_reconnect=True,
-    connection_retries=None
-)
-r = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
+    connection_retries=None)
+r = redis.Redis(
+    host="localhost",
+    port=6379,
+    db=0,
+    decode_responses=True)
+BUTTON_BOT.start(bot_token=bot_token)
+print("BUTTON_BOT is running!")
+BUTTON_BOT.run_until_disconnected()
