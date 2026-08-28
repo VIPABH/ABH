44c159b7c478e88376aaae56fa7ade857286c277
VIPABH <a891983.2@gmail.com>
2026-08-28 15:33:36 +0300

    Update client.py

--- client.py
+++ client.py
@@ -2,9 +2,16 @@ from telethon import events, TelegramClient, connection
 import os, redis
 api_id = int(os.getenv("API_ID"))
 api_hash = os.getenv("API_HASH")
-bot_token = os.getenv("BUTTON_BOT")
 BUTTON_BOT = TelegramClient(
     "BUTTON_BOT",
+    api_id,
+    api_hash,
+    connection=connection.ConnectionTcpFull,
+    sequential_updates=False,
+    auto_reconnect=True,
+    connection_retries=None)
+REACTBOT = TelegramClient(
+    "REACTBOT",
     api_id,
     api_hash,
     connection=connection.ConnectionTcpFull,
