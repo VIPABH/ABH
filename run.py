def50ebb3548e6991e75c0dd1049bb348b8a29da
VIPABH <a891983.2@gmail.com>
2026-09-02 12:06:41 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,16 @@
+import os
+import asyncio
 from buttonbot.button_bot import *
 from react import *
-from client  import *
-import os
-BUTTON_BOT.start(bot_token=os.getenv("BUTTON_BOT"))
-print("BUTTON_BOT is running!")
-REACTBOT.start(bot_token=os.getenv("REACTBOT"))
-print("REACTBOT is running!")
-REACTBOT.run_until_disconnected()
+from client import *
+async def main():
+    await BUTTON_BOT.start(bot_token=os.getenv("BUTTON_BOT"))
+    print("BUTTON_BOT is running!")
+    await REACTBOT.start(bot_token=os.getenv("REACTBOT"))
+    print("REACTBOT is running!")
+    await asyncio.gather(
+        BUTTON_BOT.run_until_disconnected(),
+        REACTBOT.run_until_disconnected()
+    )
+if __name__ == "__main__":
+    asyncio.run(main())
