121716e8956d417fba2d6d9b8e8c91b0bd4d908a
VIPABH <a891983.2@gmail.com>
2026-09-02 14:30:08 +0300

    Update run.py

--- run.py
+++ run.py
@@ -4,12 +4,10 @@ from buttonbot.button_bot import *
 from react import *
 from client import *
 async def main():
-    print("Starting bots...")
-    await asyncio.gather(
-        BUTTON_BOT.start(bot_token=os.getenv("BUTTON_BOT")),
-        REACTBOT.start(bot_token=os.getenv("REACTBOT"))
-    )
-    print("BUTTON_BOT and REACTBOT are now running!")
+    await BUTTON_BOT.start(bot_token=os.getenv("BUTTON_BOT"))
+    print("BUTTON_BOT is running!")
+    await REACTBOT.start(bot_token=os.getenv("REACTBOT"))
+    print("REACTBOT is running!")
     await asyncio.gather(
         BUTTON_BOT.run_until_disconnected(),
         REACTBOT.run_until_disconnected()
