9016d4f2b5f1a9c2397fb288d8db4633802f38fd
VIPABH <a891983.2@gmail.com>
2026-09-02 15:55:16 +0300

    Update start.py

--- react/start.py
+++ react/start.py
@@ -2,30 +2,25 @@ from helpers import *
 from .ABHS import *
 import asyncio
 rules = [
-    'ممنوع تنقل ملكية لأي حساب من حساباتي', 
+    'ممنوع تنقل ملكية لأي حساب من الحسابات', 
     'ممنوع تضيف اي حساب مو مضاف مسبقا',
-    'ممنوع تنزل اي حساب من الاشراف',
+    'ممنوع تطرد اي حساب ',
 ]
 text = f'**قوانين البوت!**\n'
 for num, rule in enumerate(rules, start=1):
     text += f'{num}- `{rule}`\n'
-text += 'البوت ممجاني بالكامل كل ما عليك الالتزام بالقوانين وفقط\n واذا حبيت تحذف التفاعلات او تطرد حساب معين انتقل الى قسم الاعدادات وهناك خصص مجموعتك مثل ما تحب'
+text += 'البوت ممجاني بالكامل كل ما عليك الالتزام بالقوانين وفقط\n واذا حبيت تحذف التفاعلات او تطرد حساب معين انتقل الى قسم الاعدادات وهناك خصص قناتك مثل ما تحب'
 session = {}
-@REACTBOT.on(events.NewMessage)
-async def is_user_check(e):
-    user = await is_user(e, REACTBOT)
-    if not user:
-        raise events.StopPropagation
 @REACTBOT.on(events.NewMessage(pattern=r'^/start'))
 async def start(e):
     id = e.sender_id
     if id in session:return
     session[id] = 'rules'
     await e.reply(f'اهلا عزيزي ( {await ment(e)} ) اني بوت رياكشن \n وظيفتي اسوي تفاعلات على المسجات ب قناتك')
-    await asyncio.sleep(3)
+    await asyncio.sleep(3.5)
     await e.respond(f'استخدامي سلس و واضح و بسيط فقط كل ما عليك فقط الالتزام بالقوانين')
-    await asyncio.sleep(2.2)
+    await asyncio.sleep(3)
     await e.respond(text)
-    await asyncio.sleep(7)
+    await asyncio.sleep(10)
     await e.respond('التزم بالقوانين واستمتع ب استخدامك للبوت, صنع ب حب بواسطة المبدع @k_4x1 ❤')
     del session[id]
