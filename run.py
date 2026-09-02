7ca0a28a0218e4e9d4eed48431505e800cf5fe23
VIPABH <a891983.2@gmail.com>
2026-09-02 12:05:57 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from buttonbot.button_bot import *
 from react import *
 from client  import *
+import os
 BUTTON_BOT.start(bot_token=os.getenv("BUTTON_BOT"))
 print("BUTTON_BOT is running!")
 REACTBOT.start(bot_token=os.getenv("REACTBOT"))
