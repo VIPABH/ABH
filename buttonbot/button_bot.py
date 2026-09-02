302c40504466276ccaf298b9a66402ca7bfece89
VIPABH <a891983.2@gmail.com>
2026-09-02 11:20:12 +0300

    Update button_bot.py

--- buttonbot/button_bot.py
+++ buttonbot/button_bot.py
@@ -4,14 +4,14 @@ import re
 @BUTTON_BOT.on(events.NewMessage(pattern=r'^/start'))
 async def start(e):
     if not e.is_private:return
-    photo = await get_profile_photo(e)
+    photo = await get_profile_photo(e, BUTTON_BOT)
     if photo:
         await BUTTON_BOT.send_file(e.chat_id, file=photo, caption=f'اهلا عزيزي ( {await ment(e)} ) اني بوت مال ازرار استخدامي سهل و بسيط ارسل `الاوامر`', reply_to=e.id)
     else:
         await BUTTON_BOT.send_message(e.chat_id, message=f'اهلا عزيزي ( {await ment(e)} ) اني بوت مال ازرار استخدامي سهل و بسيط \n ارسل `الاوامر`', reply_to=e.id)
 @BUTTON_BOT.on(events.NewMessage)
 async def is_user_check(e):
-    user = await is_user(e)
+    user = await is_user(e, BUTTON_BOT)
     if not user:
         raise events.StopPropagation
 @BUTTON_BOT.on(events.NewMessage(pattern=r'^الاوامر$'))
