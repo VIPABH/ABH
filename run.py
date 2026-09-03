3d1820d792583101acbbfb34d583f025142d30c1
VIPABH <a891983.2@gmail.com>
2026-09-03 10:18:12 +0300

    Update run.py

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import os
 import asyncio
 import sys
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-from .client import *
+from client import *
 import buttonbot.run
 import react.run
 async def main():
