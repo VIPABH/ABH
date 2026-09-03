c6f71e68ccbe9123a6eea98a7d16b808cea279fb
VIPABH <a891983.2@gmail.com>
2026-09-03 10:14:52 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,16 +1,23 @@
 import os
 import asyncio
-from buttonbot.button_bot import *
-from client import *
-from react import *
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+from buttonbot.client import BUTTON_BOT
+from react.client import REACTBOT
+import buttonbot.run
+import react.run
 async def main():
+    print("⏳ جاري تشغيل البوتين...")
     await BUTTON_BOT.start(bot_token=os.getenv("BUTTON_BOT"))
-    print("BUTTON_BOT is running!")
+    print("✅ BUTTON_BOT is running!")
     await REACTBOT.start(bot_token=os.getenv("REACTBOT"))
-    print("REACTBOT is running!")
+    print("✅ REACTBOT is running!")
     await asyncio.gather(
         BUTTON_BOT.run_until_disconnected(),
         REACTBOT.run_until_disconnected()
     )
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except (KeyboardInterrupt, SystemExit):
+        print("\n🛑 تم إيقاف تشغيل البوتين.")
