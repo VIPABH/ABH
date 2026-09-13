ce2d42d6913b0b5496cdeecad9a5a9fd9239fdf1
VIPABH <a891983.2@gmail.com>
2026-09-13 13:44:58 +0300

    Update run.py

--- react/run.py
+++ react/run.py
@@ -2,7 +2,7 @@ import os
 import sys
 import asyncio
 from ABHS import init_clients, REACTBOT, mainABH, bot, ABHS
-import react.الحماية
+import الحماية
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 async def main():
     print("⏳ جاري تشغيل بوت التفاعلات والـ 15 عميل...")
