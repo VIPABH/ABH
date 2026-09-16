15b2a358f66f52882405ed364a9e1e83cd7d100e
VIPABH <a891983.2@gmail.com>
2026-09-16 09:57:56 +0300

    Update run.py

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from client import BUTTON_BOT, REACTBOT
 import buttonbot
 import react
-from react.run import *
+from react import *
 async def main():
     token_button = os.getenv("BUTTON_BOT")
     token_react = os.getenv("REACTBOT")
