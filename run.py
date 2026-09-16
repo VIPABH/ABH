cf7df3dc3d12163f9cf7d5d59d9da130d0bc1a7e
VIPABH <a891983.2@gmail.com>
2026-09-16 09:31:00 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,15 +1,25 @@
 import os
 import asyncio
 import sys
+import logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.ERROR
+)
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-from client import *
+from client import BUTTON_BOT, REACTBOT
 import react
 import buttonbot
 async def main():
+    token_button = os.getenv("BUTTON_BOT")
+    token_react = os.getenv("REACTBOT")
+    if not token_button or not token_react:
+        logging.error("❌ تعذر العثور على توكنات البوتات في متغيرات البيئة (Environment Variables)!")
+        return
     print("⏳ جاري تشغيل البوتين...")
-    await BUTTON_BOT.start(bot_token=os.getenv("BUTTON_BOT"))
+    await BUTTON_BOT.start(bot_token=token_button)
     print("✅ BUTTON_BOT is running!")
-    await REACTBOT.start(bot_token=os.getenv("REACTBOT"))
+    await REACTBOT.start(bot_token=token_react)
     print("✅ REACTBOT is running!")
     await asyncio.gather(
         BUTTON_BOT.run_until_disconnected(),
