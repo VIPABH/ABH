b3d0bc4ca0ba19d2dae7a75a128dddd6fc1f5f24
VIPABH <a891983.2@gmail.com>
2026-09-16 09:28:17 +0300

    Update run.py

--- run.py
+++ run.py
@@ -4,15 +4,16 @@ import sys
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from client import *
 import react
+import buttonbot
 async def main():
     print("⏳ جاري تشغيل البوتين...")
     await BUTTON_BOT.start(bot_token=os.getenv("BUTTON_BOT"))
     print("✅ BUTTON_BOT is running!")
-    # await REACTBOT.start(bot_token=os.getenv("REACTBOT"))
+    await REACTBOT.start(bot_token=os.getenv("REACTBOT"))
     print("✅ REACTBOT is running!")
     await asyncio.gather(
         BUTTON_BOT.run_until_disconnected(),
-        # REACTBOT.run_until_disconnected()
+        REACTBOT.run_until_disconnected()
     )
 if __name__ == "__main__":
     try:
