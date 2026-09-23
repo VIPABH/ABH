8ee276bf0356f918542075d868a89c72a0f8c528
VIPABH <a891983.2@gmail.com>
2026-09-23 08:26:51 +0300

    Update run.py

--- react/run.py
+++ react/run.py
@@ -2,13 +2,8 @@ import os
 import sys
 import asyncio
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-from ABHS import *
+from ABHS import init_clients, ABHS, REACTBOT
 async def run_react():
     print("⏳ جاري تشغيل بوت التفاعلات والـ 15 عميل...")
-    await init_clients()    
-    await REACTBOT.start()
-    if isinstance(ABHS, list):
-        tasks = [client.start() for client in ABHS if client and not client.is_connected()]
-        if tasks:
-            await asyncio.gather(*tasks)
-    print("🚀 جميع الحسابات وبوت التفاعلات شغالين بنجاح!")
+    await init_clients()
+    print(f"🚀 تم تشغيل بوت التفاعلات و ({len(ABHS)}) حساب بنجاح!")
