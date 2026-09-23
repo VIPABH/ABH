24050aa3ea5523f7cd4b33dd0f03a4fefaa1917e
VIPABH <a891983.2@gmail.com>
2026-09-23 08:24:18 +0300

    Update client.py

--- react/client.py
+++ react/client.py
@@ -1,21 +1,21 @@
-# import os
-# import redis
-# from telethon import TelegramClient, connection
-# api_id_env = os.getenv("API_ID")
-# api_id = int(api_id_env) if api_id_env else None
-# api_hash = os.getenv("API_HASH")
-# r = redis.Redis(
-#     host="localhost",
-#     port=6379,
-#     db=0,
-#     decode_responses=True
-# )
-# REACTBOT = TelegramClient(
-#     "REACTBOT",
-#     api_id,
-#     api_hash,
-#     connection=connection.ConnectionTcpFull,
-#     sequential_updates=False,
-#     auto_reconnect=True,
-#     connection_retries=None
-# )
+import os
+import redis
+from telethon import TelegramClient, connection
+api_id_env = os.getenv("API_ID")
+api_id = int(api_id_env) if api_id_env else None
+api_hash = os.getenv("API_HASH")
+r = redis.Redis(
+    host="localhost",
+    port=6379,
+    db=0,
+    decode_responses=True
+)
+REACTBOT = TelegramClient(
+    "REACTBOT",
+    api_id,
+    api_hash,
+    connection=connection.ConnectionTcpFull,
+    sequential_updates=False,
+    auto_reconnect=True,
+    connection_retries=None
+)
